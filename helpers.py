3b71fc8e87b20cce59ccd50deb1a34917fd21bb3
Anastasia <asya_apto@mail.ru>
2022-02-03 16:25:45 +0300

    Download books from all pages

---
+++ helpers.py
@@ -0,0 +1,66 @@
+import os
+
+import requests
+
+from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlsplit, unquote
+
+
+def check_for_redirect(response):
+    for resp in response.history:
+        if resp.status_code == 302:
+            raise requests.HTTPError
+
+
+def download_book(book_id, dir_name, book_title):
+    endpoint = "https://tululu.org/txt.php"
+    payload = {"id": book_id}
+    response = requests.get(endpoint, params=payload, verify=False)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    filename = "{}.{}.txt".format(book_id, book_title)
+    safe_filename = sanitize_filename(filename)
+    book_path = os.path.join(dir_name, safe_filename)
+    with open(book_path, "wt", encoding='utf-8') as file:
+        file.write(response.text)
+    return book_path
+
+
+def download_cover(url, dir_name):
+    response = requests.get(url)
+    response.raise_for_status()
+    img_name = urlsplit(unquote(url)).path.split("/")[-1]
+    img_path = os.path.join(dir_name, img_name)
+    with open(img_path, "wb") as file:
+        file.write(response.content)
+    return img_path
+
+
+def parse_book_page(soup, book_url):
+    book_h1 = soup.select_one("#content h1").text
+    book_title, book_author = book_h1.split("::")
+    cover_src = soup.select_one(".bookimage img")
+    img_url = urljoin(book_url, cover_src["src"]) if cover_src else None
+    comments = soup.select(".texts .black")
+    genres = soup.select(".d_book > a")
+    return {
+        "title": book_title.strip(),
+        "author": book_author.strip(),
+        "cover_url": img_url,
+        "book_url": book_url,
+        "comments": [comment.text for comment in comments],
+        "genres": [genre.text for genre in genres]
+    }
+
+
+def create_description(parsed_book, img_path, book_path):
+    description = {
+        "title": parsed_book["title"],
+        "author": parsed_book["author"],
+        "img_src": img_path,
+        "book_path": book_path,
+        "comments": parsed_book["comments"],
+        "genres": parsed_book["genres"]
+    }
+    return description
---
+++ parse_tululu_category.py
@@ -0,0 +1,62 @@
+import argparse
+import json
+import os
+import urllib3
+import re
+
+import requests
+
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+from helpers import (check_for_redirect, create_description,download_book,
+                     download_cover, parse_book_page)
+
+
+if __name__ == "__main__":
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    category_url = "http://tululu.org/l55/"
+    endpoint = "https://tululu.org"
+    num_pages = 4
+
+    books_description = {}
+
+    for page in range(num_pages):
+        books_dir_name = "books"
+        img_dir_name = "images"
+        os.makedirs(books_dir_name, exist_ok=True)
+        os.makedirs(img_dir_name, exist_ok=True)
+
+        bs4_response = requests.get(urljoin(category_url, str(page+1)))
+        bs4_response.raise_for_status()
+        soup = BeautifulSoup(bs4_response.text, "lxml")
+
+        book_cards = soup.select(".d_book .bookimage a")
+        for book_card in book_cards:
+            book_path = book_card["href"]
+            book_url = urljoin(endpoint, book_path)
+            book_id = re.sub("[^0-9]", "", book_path)
+
+            try:
+                bs4_response = requests.get(book_url)
+                bs4_response.raise_for_status()
+                check_for_redirect(bs4_response)
+                soup = BeautifulSoup(bs4_response.text, "lxml")
+
+                parsed_book = parse_book_page(soup, book_url)
+                book_path = download_book(book_id, books_dir_name,
+                                          parsed_book["title"])
+                book_cover = parsed_book["cover_url"]
+                if book_cover:
+                    img_path = download_cover(book_cover, img_dir_name)
+                books_description[book_id] = create_description(parsed_book,
+                                                                img_path,
+                                                                book_path)
+            except requests.HTTPError:
+                pass
+            except requests.exceptions.ConnectionError as error:
+                print(error)
+
+    with open("books_description.json", "w", encoding="utf8") as file:
+        json.dump(books_description, file, ensure_ascii=False, indent=4)
+
