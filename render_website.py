2523d5e3ac7680f6b7e9beda040d67f390290f92
Anastasia <asya_apto@mail.ru>
2022-02-15 23:40:16 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,9 +1,11 @@
 import io
 import json
+import os
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server, shell
+from more_itertools import chunked
 
 
 def reload_site():
@@ -16,16 +18,30 @@ def reload_site():
     with io.open("description.json", encoding="utf-8") as file:
         books_description = json.load(file)
 
-    rendered_page = template.render(
-        books=books_description,
-    )
-    with open("index.html", "w", encoding="utf8") as file:
-        file.write(rendered_page)
+    books_ids = [book_id for book_id, book_description in books_description.items()]
+    books_groups = list(chunked(books_ids, 20))
+
+    for page_num in range(len(books_groups)):
+        books_group = books_groups[page_num]
+        books = {book_id: books_description[book_id] for book_id in books_group}
+        rendered_page = template.render(
+            books=books,
+        )
+
+        with open(os.path.join("pages", f"index{page_num}.html"), "w",
+                  encoding="utf8") as file:
+            file.write(rendered_page)
 
 
-reload_site()
-server = Server()
-server.watch('template.html', reload_site)
-server.serve(root='.')
+def main():
+    os.makedirs("pages", exist_ok=True)
+    reload_site()
+    server = Server()
+    server.watch("template.html", reload_site)
+    server.serve(default_filename="pages/index0.html")
+
+
+if __name__ == "__main__":
+    main()
 
 
